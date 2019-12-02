cf988b3222a1b4384fe312a572b997c2765eaffe
Jakub Pištěk <mail@jakubpistek.cz>
2019-12-02 18:18:59 +0700

    Use promises instead of callbacks

--- src/fuel.ts
+++ src/fuel.ts
@@ -1,21 +1,23 @@
-
-import { readFile } from 'fs';
+import { promises } from 'fs';
 
 function fuel(mass: number): number {
-    const fuelForMass = Math.floor(mass / 3) - 2;
+  const fuelForMass = Math.floor(mass / 3) - 2;
 
-    return fuelForMass > 0 ? fuelForMass + fuel(fuelForMass) : 0;
+  return fuelForMass > 0 ? fuelForMass + fuel(fuelForMass) : 0;
 }
 
 function sum(a: number, b: number) {
-    return a + b;
+  return a + b;
 }
 
-readFile('data/modules.data', (err, data: Buffer) => {
-    if (err) throw err;
+promises.readFile('data/modules.data')
+    .then((data: Buffer) => {
 
-    const modules = data.toString().split("\n").map(val => parseInt(val));
+      const modules = data.toString().split('\n').map(val => parseInt(val));
 
-    const fjul = modules.map(fuel).reduce(sum, 0);
-    console.log('Total Fuel', fjul);
-});
+      const fjul = modules.map(fuel).reduce(sum, 0);
+      console.log('Total Fuel', fjul);
+    })
+    .catch(err => {
+      throw err;
+    });
--- src/program.ts
+++ src/program.ts
@@ -1,88 +1,89 @@
-
-import { readFile } from 'fs';
+import { promises } from 'fs';
 
 function doInstruction(operation: number, noun: number, verb: number) {
 
-    if (operation === 1) {
-        return sum(noun, verb);
-    } else {
-        return multi(noun, verb);
-    }
+  if (operation === 1) {
+    return sum(noun, verb);
+  } else {
+    return multi(noun, verb);
+  }
 }
 
 function storeResult(memory: number[], position: number, result: number) {
-    const newMemory = [...memory];
-    newMemory[position] = result;
-    return newMemory;
+  const newMemory = [...memory];
+  newMemory[position] = result;
+  return newMemory;
 }
 
 function sum(a: number, b: number): number {
-    return a + b;
+  return a + b;
 }
 
 function multi(a: number, b: number): number {
-    return a * b;
+  return a * b;
 }
 
 function checkResult(result: number) {
-    return result === 19690720;
+  return result === 19690720;
 }
 
 function calculateProgram(memory: number[], noun: number, verb: number) {
 
-    memory = [...memory];
-    let operation = memory[0];
-    let instructionPointer = 0;
-    let result = null;
-    memory[1] = noun;
-    memory[2] = verb;
+  memory = [...memory];
+  let operation = memory[0];
+  let instructionPointer = 0;
+  let result = null;
+  memory[1] = noun;
+  memory[2] = verb;
 
-    do {
-        const instruction = memory.slice(instructionPointer, instructionPointer + 4);
+  do {
+    const instruction = memory.slice(instructionPointer, instructionPointer + 4);
 
-        const resultPosition = instruction[3];
+    const resultPosition = instruction[3];
 
-        result = doInstruction(operation, memory[instruction[1]], memory[instruction[2]]);
-        memory = storeResult(memory, resultPosition, result);
+    result = doInstruction(operation, memory[instruction[1]], memory[instruction[2]]);
+    memory = storeResult(memory, resultPosition, result);
 
-        instructionPointer = instructionPointer + 4;
-        operation = memory[instructionPointer];
+    instructionPointer = instructionPointer + 4;
+    operation = memory[instructionPointer];
 
-    } while (operation !== NOOP);
+  } while (operation !== NOOP);
 
-    return result;
+  return result;
 }
 
 const NOOP = 99;
 const MAX_OPCODE = 99;
 
-readFile('data/opcodes.data', (err, data: Buffer) => {
-    if (err) throw err;
+promises.readFile('data/opcodes.data')
+    .then((data: Buffer) => {
+      const memory = data.toString().split(',').map(val => parseInt(val));
 
-    const memory = data.toString().split(",").map(val => parseInt(val));
-
-    let noun = 0;
-    let verb = 0;
-    let result = null;
-    do {
+      let noun = 0;
+      let verb = 0;
+      let result = null;
+      do {
         verb = 0;
         do {
-            result = calculateProgram(memory, noun, verb);
-            if (!checkResult(result)) {
-                verb++;
-            }
+          result = calculateProgram(memory, noun, verb);
+          if (!checkResult(result)) {
+            verb++;
+          }
         } while (verb <= MAX_OPCODE && !checkResult(result));
 
         if (!checkResult(result)) {
-            noun++;
+          noun++;
         }
-    } while (noun <= MAX_OPCODE && !checkResult(result));
+      } while (noun <= MAX_OPCODE && !checkResult(result));
 
-    if (checkResult(result)) {
+      if (checkResult(result)) {
         console.log('Program result is', noun * 100 + verb);
-    } else {
+      } else {
         console.log('no result found');
-    }
+      }
 
-});
+    })
+    .catch(err => {
+      throw err;
+    });
 
